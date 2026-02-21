b078f9fed3bf2273386411cbb9214137789d3fb5
ZorayaF <azflorez@uniboyaca.edu.co>
2026-02-21 12:29:08 -0500

    chore(config): implement path aliases for modular architecture

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // vite.config.js
+  resolve: {
+    alias: {
+      '@core': path.resolve(__dirname, './src/core'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  }
 })
